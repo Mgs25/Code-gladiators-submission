ec51447b4387e36ebe70262650e29a64d187e264
Mgs25 <62834534+Mgs25@users.noreply.github.com>
2021-07-14 15:57:56 +0530

    Update PUSHPOP.c

--- PUSHPOP.c
+++ PUSHPOP.c
@@ -2,7 +2,7 @@
 /*                                                                    */
 /* A push and pop function for a stack of integers                    */
 /*--------------------------------------------------------------------*/
-#include "CALC.h"
+#include "calc.h"
 #include <stdlib.h>
 
 /*--------------------------------------------------------------------*/
@@ -11,23 +11,23 @@
 /* action: get a link to hold the pushed value, push link on stack    */
 /*                                                                    */
 void push(IntStack* stk, const int num) {                                   // Improved const-correctness, number is not modified in push function
-  IntLink* ptr;
-  ptr       = (IntLink*)malloc(sizeof(IntLink));  /*  PUSHPOP1   */         // Replaced illegal character '-', '/' with 
-  ptr->i    = num;                                /*  PUSHPOP2   */
-  ptr->next = stk->top;
-  stk->top  = ptr;
+    IntLink* ptr;
+    ptr       = (IntLink*)malloc(sizeof(IntLink));                          // Replaced illegal character '-', '/' with
+    ptr->i    = num;
+    ptr->next = stk->top;
+    stk->top  = ptr;
 }
 
 /*--------------------------------------------------------------------*/
 /* return: int value popped from stack                                */
 /* action: pops top element from stack and gets return value from it  */
 /*--------------------------------------------------------------------*/
-const int pop(IntStack* stk) {
-  IntLink* ptr;
-  int num;
-  ptr      = stk->top;
-  num      = ptr->i;
-  stk->top = ptr->next;
-  free(ptr);
-  return num;
+int pop(IntStack* stk) {
+    IntLink* ptr;
+    int num;
+    ptr      = stk->top;
+    num      = ptr->i;
+    stk->top = ptr->next;
+    free(ptr);
+    return num;
 }
