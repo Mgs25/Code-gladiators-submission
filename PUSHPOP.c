652961f48878e33b865f1f2813b5fb36f1b1649a
Methran GS <62834534+Mgs25@users.noreply.github.com>
2021-07-14 16:23:52 +0530

    Update PUSHPOP.c

--- PUSHPOP.c
+++ PUSHPOP.c
@@ -10,9 +10,18 @@
 /*         num - value to push on the stack                           */
 /* action: get a link to hold the pushed value, push link on stack    */
 /*                                                                    */
+
+/*--------------------------------------------------------------------*/
+/* CHANGES IN "PUSHPOP.c"                                             */
+/* -> changed push function's num paratermeter from int to const int  */
+/*    as the integer is constant in the push function                 */
+/* -> Replaced all occurrences of illegal '–' and '⁄' with '-' and '/'*/
+/*    respectively, to avoid stray errors                             */
+/*--------------------------------------------------------------------*/
+
 void push(IntStack* stk, const int num) {                                   // Improved const-correctness, num is not modified in push function
     IntLink* ptr;
-    ptr       = (IntLink*)malloc(sizeof(IntLink));                          // Replaced illegal character '-', '/' with
+    ptr       = (IntLink*)malloc(sizeof(IntLink));                          // Replaced illegal character '–', '/' with '-', '/'
     ptr->i    = num;
     ptr->next = stk->top;
     stk->top  = ptr;
